5f50e1c9b3f27050e9b91e89cf3646a72c09bee0
arfanovi <arfanovi.gub1@gmail.com>
2024-06-28 22:51:02 +0600

    function power calculate

---
+++ index.js
@@ -0,0 +1,12 @@
+
+
+// 1. Function to Calculate Power
+
+// Declare Function | function name circuitPower()
+
+function circuitPower(voltage, current){
+    return voltage * current;
+}
+// console.log()
+const input = circuitPower(10,10);
+console.log(input)
